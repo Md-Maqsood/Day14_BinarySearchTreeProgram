c5dc7d650b67f9f956523386d012d9facc056d03
maqsood <maqsoodhamd37@gmail.com>
2020-10-15 20:44:25 +0530

    [Maqsood].add: initial commit

---
+++ src/main/java/com/bridgeLabs/binarySearchTreeProgram/BinarySearchTree.java
@@ -0,0 +1,12 @@
+package com.bridgeLabs.binarySearchTreeProgram;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class BinarySearchTree {
+	private static final Logger logger = LogManager.getLogger(BinarySearchTree.class);
+
+	public static void main(String[] args) {
+		logger.debug("Welcome to binary search tree program.");
+	}
+}
---
+++ src/test/java/com/bridgeLabs/binarySearchTreeProgram/BinarySearchTreeTest.java
@@ -0,0 +1,20 @@
+package com.bridgeLabs.binarySearchTreeProgram;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class BinarySearchTreeTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
